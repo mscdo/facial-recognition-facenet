c70eb73f55d92611ee41683ef5015086ef16f49d
Melina Dall'Orto <melscdo@gmail.com>
2023-03-23 16:27:20 -0300

    Few changes and test. Do not implement

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
 import mtcnn
-print(mtcnn.__version__)
-
+# print(mtcnn.__version__)
+import os
 import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import cv2 # opencv
 from mtcnn.mtcnn import MTCNN
-from matplotlib import pyplot as plt
+import cv2
+import matplotlib as plt
 from keras.models import load_model
 from PIL import Image
+from werkzeug.utils import secure_filename
+from flask import Flask, jsonify, render_template,  flash, request, redirect, url_for, session, send_from_directory
 
+UPLOAD_FOLDER = 'target/person'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-import os
-
-
-
-# Input data files are available in the "./input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
-
-
-# print(os.listdir("./input"))
-
-# # Any results you write to the current directory are saved as output.
-# img = cv2.imread('./input/data/train/melina.jpeg')
-# plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
-# #plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-# plt.show()
-# print(img.shape)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 # extract a single face from a given photograph
@@ -53,18 +41,14 @@ def extract_face(filename, required_size=(160, 160)):
     face_array = np.asarray(image)
     return face_array
 
-# load the photo and extract the face
-# pixels = extract_face('./input/data/train/melina/melina.jpeg')
-# plt.imshow(pixels)
-# plt.show()
-# print(pixels.shape)
+
 
 def load_face(dir):
     faces = list()
     # enumerate files
     for filename in os.listdir(dir):
         path = dir + filename
-        print('load face path : ', path)
+        # print('load face path : ', path)
         face = extract_face(path)
         faces.append(face)
     return faces
@@ -75,37 +59,14 @@ def load_dataset(dir):
     for subdir in os.listdir(dir):
         path = dir + subdir + '/'
         # path = dir 
-        print('PATH  :   ', path)
+        # print('PATH  :   ', path)
         faces = load_face(path)
         labels = [subdir for i in range(len(faces))]
-        print("loaded %d sample for class: %s" % (len(faces),subdir) ) # print progress
+        # print("loaded %d sample for class: %s" % (len(faces),subdir) ) # print progress
         X.extend(faces)
         y.extend(labels)
     return np.asarray(X), np.asarray(y)
 
-# load train dataset
-trainX, trainy = load_dataset('./input/data/train/')
-print(trainX.shape, trainy.shape)
-# load test dataset
-testX, testy = load_dataset('./input/data/test/')
-print(testX.shape, testy.shape)
-
-
-# save and compress the dataset for further use
-np.savez_compressed('geocontrol.npz', trainX, trainy, testX, testy)
-
-# load the face dataset
-data = np.load('geocontrol.npz')
-trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
-
-
-print('Loaded: ', trainX.shape, trainy.shape, testX.shape, testy.shape)
-
-
-
-# load the facenet model
-facenet_model = load_model('./models/facenet_keras.h5')
-print('Loaded Model')
 
 
 
@@ -123,39 +84,92 @@ def get_embedding(model, face):
 
 
 
-   
-# convert each face in the train set into embedding
-emdTrainX = list()
-for face in trainX:
-    emd = get_embedding(facenet_model, face)
-    emdTrainX.append(emd)
+
+
+def run_train_dataset():
+    # load train dataset
+    trainX, trainy = load_dataset('input/data/train/')
+    # print(trainX.shape, trainy.shape)
     
+    # save and compress the dataset for further use
+    np.savez_compressed('geocontrol_train.npz', trainX, trainy)
     
-emdTrainX = np.asarray(emdTrainX)
-print('EMDTRAINX ************************************', emdTrainX.shape)
-
-
-
-# convert each face in the test set into embedding
-emdTestX = list()
-for face in testX:
-    emd = get_embedding(facenet_model, face)
-    emdTestX.append(emd)
+    data = np.load('geocontrol_train.npz')
+    trainX, trainy = data['arr_0'], data['arr_1']
+    # print('Loaded: ', trainX.shape, trainy.shape)
     
-emdTestX = np.asarray(emdTestX)
-print('EMDTESTX ************************************', emdTestX.shape)
+    # load the facenet model
+    facenet_model = load_model('models/facenet_keras.h5')
+    # print('Loaded Model')
+    
+    # convert each face in the train set into embedding
+    emdTrainX = list()
+    for face in trainX:
+        emd = get_embedding(facenet_model, face)
+        emdTrainX.append(emd)
+        
+        
+    emdTrainX = np.asarray(emdTrainX)
+    # print('EMDTRAINX ************************************', emdTrainX.shape)
+    
+    # save arrays to one file in compressed format
+    np.savez_compressed('geocontrol-embeddings_train.npz', emdTrainX, trainy)
+    return  emdTrainX, trainy
 
-# save arrays to one file in compressed format
-np.savez_compressed('geocontrol-embeddings.npz', emdTrainX, trainy, emdTestX, testy)
+
+def run_test_dataset():
+    # load test dataset
+    testX, testy = load_dataset('./target/')
+    # print(testX.shape, testy.shape)
+    
+    # save and compress the dataset for further use
+    np.savez_compressed('geocontrol_test.npz', testX, testy)
+    
+    # load the face dataset
+    data = np.load('geocontrol_test.npz')
+    testX, testy = data['arr_0'], data['arr_1']
+    # print('Loaded: ', testX.shape, testy.shape)
+    
+
+    # load the facenet model
+    facenet_model = load_model('models/facenet_keras.h5')
+    # print('Loaded Model')
+    
+    # convert each face in the test set into embedding
+    emdTestX = list()
+    for face in testX:
+        emd = get_embedding(facenet_model, face)
+        emdTestX.append(emd)
+        
+    emdTestX = np.asarray(emdTestX)
+    # print('EMDTESTX ************************************', emdTestX.shape)
+
+    # save arrays to one file in compressed format
+    np.savez_compressed('geocontrol-embeddings_test.npz', emdTestX, testy)
+    # print('EMDTESTX ************************************')
+    # print(emdTestX)
+    # print('TESTY ************************************')
+    # print(testy)
+    return  emdTestX, testy
 
 
-
-def opcao_um():
+def run_test():
     from sklearn.metrics import accuracy_score
     from sklearn.preprocessing import LabelEncoder
     from sklearn.preprocessing import Normalizer
     from sklearn.svm import SVC
 
+
+    data = np.load('geocontrol-embeddings_test.npz')
+    emdTrainX, trainy = data['arr_0'], data['arr_1']
+    
+    data = np.load('geocontrol-embeddings_train.npz')
+    emdTestX, testy = data['arr_0'], data['arr_1']
+    
+      
+    # load test dataset
+    testX, testy = load_dataset('input/data/test/')
+    print(testX.shape, testy.shape)
     print("Dataset: train=%d, test=%d" % (emdTrainX.shape[0], emdTestX.shape[0]))
     # normalize input vectors
     in_encoder = Normalizer()
@@ -176,7 +190,7 @@ def opcao_um():
     score_train = accuracy_score(trainy_enc, yhat_train)
     score_test = accuracy_score(testy_enc, yhat_test)
     # summarize
-    print('Accuracy: train=%.3f, test=%.3f' % (score_train*100, score_test*100))
+    # print('Accuracy: train=%.3f, test=%.3f' % (score_train*100, score_test*100))
 
 
     from random import choice 
@@ -202,13 +216,70 @@ def opcao_um():
     class_probability = yhat_prob[0,class_index] * 100
     predict_names = out_encoder.inverse_transform(yhat_class)
     all_names = out_encoder.inverse_transform([0,1,2])
-    print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
+    predicted = 'Predicted: %s (%.3f)' % (predict_names[0], class_probability)
     # print('Predicted: \n%s \n%s' % (all_names, yhat_prob[0]*100))
-    print('Expected: %s' % random_face_name[0])
+    expected = 'Expected: %s' % random_face_name[0]
+    return jsonify(predicted, expected)
     # plot face
-    plt.imshow(random_face)
-    title = '%s (%.3f)' % (predict_names[0], class_probability)
-    plt.title(title)
-    plt.show()
+    # plt.imshow(random_face)
+    # title = '%s (%.3f)' % (predict_names[0], class_probability)
+    # plt.title(title)
+    # plt.show()
+     
+def run():
+    emdTrainX, trainy = run_train_dataset()
+    emdTestX, testy = run_test_dataset()
+    texto = run_test()
+    return texto
+
+
+            
+
+
+app= Flask(__name__, template_folder='templates')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'geocontrol'
+
+
+@app.route('/')
+def index():
+    return 'INdex'
+
+
+
+@app.route('/adicionar')
+def adicionar():
+    return 'Oi'
+
+@app.route('/upload-image', methods=['GET', 'POST'])
+def upload_image():
+    if request.method == "POST":
+        if request.files:
+            image = request.files["image"]
+            # print(image + "Uploaded to Faces")
+            # flash('Image successfully Uploaded to Faces.')
+            image.save(os.path.join(UPLOAD_FOLDER, image.filename))
+            filename = os.path.join(UPLOAD_FOLDER, image.filename)
+            print("stored as:" + filename)
+            return render_template("upload.html", uploaded_image=filename)
+    return render_template("upload.html")
+
+@app.route('/uploads/<filename>')
+def send_uploaded_file(filename=''):
     
-opcao_um()
+    return send_from_directory(UPLOAD_FOLDER, filename)
+
+
+
+@app.route('/identificar', methods=['GET', 'POST'])
+def identificar():
+    # texto = run()
+    run_test_dataset()
+    texto = run_test()
+    return texto
+  
+
+
+# app.run(host='0.0.0.0')
+
+run()
